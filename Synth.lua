bb460c333df14b2eb3c7ce4d3af6232c5eb25868
That1DireWolf-4746 <vwgchan@gmail.com>
2025-09-13 02:41:10 -0700

    Update Synth.lua
    it should work properly now
    
    with thanks to yingcan

--- Synth.lua
+++ Synth.lua
@@ -3,13 +3,11 @@ json = require("json")
 local PACKETTIME = 20
 
 local instruments = {}
-local currentLine = {}
 local line = 1
 local songName = ""
 local song = nil
 local endsong = false
 local currentSet = ""
-local _endtime = 2
 local _nextNote = 0
 local pitch = {
 ["C"] = 261.63/369.99,
@@ -34,20 +32,7 @@ end
 
 function play(sn)
 	endsong = false
-	currentSet = ""
-	if host:isHost() then
-		songName = sn
-		song = json.decode(file:readString("music/" .. songName .. ".json"))
-		instruments = song["instruments"]
-		pings.sendInstruments(instruments)
-		nextSet()
-	end
-end
-
-function play(sn, pingtime)
-	endsong = false
-	PACKETTIME = pingtime
-	currentSet = ""
+	line = 1
 	if host:isHost() then
 		songName = sn
 		song = json.decode(file:readString("music/" .. songName .. ".json"))
@@ -60,21 +45,25 @@ end
 function stop()
 	endsong = true
 	line = 1
+	currentLine = {}
+end
+
+function clear()
 	currentSet = ""
 	_nextNote = 0
-	currentLine = {}
 end
 
 function nextSet()
 	local ticker = 0
 	local toSend = ""
 	while ticker < PACKETTIME do
-		currentLine = song["song"][line]
+		local currentLine = song["song"][line]
 		line = line + 1
 		ticker = ticker + 1
 		if song["song"][line] == nil then
+			pings.sentCurrentSet(toSend)
 			stop()
-			break
+			return nil
 		end
 		if currentLine[1] ~= nil then
 			toSend = toSend .. ";" .. tostring(ticker - 1) .. "-"
@@ -85,7 +74,6 @@ function nextSet()
 		
 	end
 	pings.sentCurrentSet(toSend)
-	ticker = 0
 end
 
 function playLine()
@@ -94,33 +82,35 @@ function playLine()
 		local toPlay = parse(play)
 		if toPlay ~= nil then
 			for _, note in ipairs(toPlay) do
-				if note ~= nil then
-					sounds:playSound("block.note_block." .. instruments[note[1]], player:getPos() + vec(0, 0.1, 0), 1, (pitch[note[2]] * 2^(note[3] - 4)), false)
-				end
+				sounds["block.note_block." .. instruments[note[1]]]:play():setPos(player:getPos() + vec(0, 0.1, 0)):setPitch((pitch[note[2]] * 2^(note[3] - 4)))
 			end
 		end
 	end
 	_nextNote = _nextNote + 1
 	if _nextNote >= PACKETTIME and not endsong then
-		nextSet()
+		if setLengthBelowLimit() then
+			nextSet()
+		end
 		_nextNote = 0
-		_endtime = 2
-		_endpos = 1
 	end
 end
 
-function parse(pos)
-	local toPlayStart = string.find(currentSet, ";" .. pos)
-	if toPlayStart == nil then
-		toPlayStart = -1
+function setLengthBelowLimit()
+	local count = 0
+	for note in string.gmatch(currentSet, ";") do
+		count = count + 1
 	end
-	local toPlayEnd = string.find(currentSet, ";", toPlayStart + 1)
+	return count < 45
+end
+
+function parse(pos)
+	local toPlayEnd = string.find(currentSet, ";", 2)
 	if toPlayEnd == nil then
 		toPlayEnd = -1
 	end
-	local toPlayString = string.sub(currentSet, toPlayStart, toPlayEnd)
+	local toPlayString = string.sub(currentSet, 1, toPlayEnd)
 	local result = {}
-	for note in string.gmatch(toPlayString, "%-%d+%a+%d+%-") do
+	for note in string.gmatch(toPlayString, "%d+%a+%d+%-") do
 		if note ~= nil then
 			local noteTable = {
 				tonumber(string.match(note, "%d+")), 
@@ -130,20 +120,18 @@ function parse(pos)
 			result[#result + 1] = noteTable 
 		end
 	end
+	currentSet = string.sub(currentSet, toPlayEnd, -1)
 	return result
 end
 
 function parseTime()
-	local startpos = _endtime
-	local nextpos = string.find(currentSet, ";", startpos + 1, false)
+	local nextpos = string.find(currentSet, "%-", 2, false)
 	if nextpos == nil then
 		nextpos = -1
 	end
-	local toPlayString = string.sub(currentSet, startpos, nextpos)
-	local lineNumber = string.match(toPlayString, "%d+")
+	local lineNumber = string.sub(currentSet, 2, nextpos - 1)
 	if tonumber(lineNumber) ~=nil then
 		if _nextNote == tonumber(lineNumber) then
-			_endtime = nextpos + 1
 			return lineNumber
 		end
 	end
@@ -155,5 +143,6 @@ function pings.sendInstruments(list)
 end
 
 function pings.sentCurrentSet(string)
-	currentSet = string
+	currentSet = currentSet .. string
+	currentSet = string.match(currentSet, ";.*")
 end
