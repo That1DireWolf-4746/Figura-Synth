5ad8dc8409eb774b1ed3000360d9944385b5e448
That1DireWolf-4746 <vwgchan@gmail.com>
2025-09-12 21:58:10 -0700

    Update Synth.lua
    redid how the pings work so it sends a bunch of lines at once and also no more tablespam

--- Synth.lua
+++ Synth.lua
@@ -1,11 +1,16 @@
 json = require("json")
 
+local PACKETTIME = 20
+
 local instruments = {}
 local currentLine = {}
 local line = 1
 local songName = ""
 local song = nil
 local endsong = false
+local currentSet = ""
+local _endtime = 2
+local _nextNote = 0
 local pitch = {
 ["C"] = 261.63/369.99,
 ["Cs"] = 277.18/369.99,
@@ -22,39 +27,133 @@ local pitch = {
 }
 
 function events.tick()
-	if string.len(songName) > 0 and endsong == false then
-		nextLine()
+	if string.len(songName) > 0 then
+		playLine()
 	end
 end
 
 function play(sn)
 	endsong = false
+	currentSet = ""
 	if host:isHost() then
 		songName = sn
 		song = json.decode(file:readString("music/" .. songName .. ".json"))
 		instruments = song["instruments"]
-		line = 1
+		pings.sendInstruments(instruments)
+		nextSet()
+	end
+end
+
+function play(sn, pingtime)
+	endsong = false
+	PACKETTIME = pingtime
+	currentSet = ""
+	if host:isHost() then
+		songName = sn
+		song = json.decode(file:readString("music/" .. songName .. ".json"))
+		instruments = song["instruments"]
+		pings.sendInstruments(instruments)
+		nextSet()
 	end
 end
 
 function stop()
 	endsong = true
+	line = 1
+	currentSet = ""
+	_nextNote = 0
+	currentLine = {}
 end
 
-function nextLine()
-	if host:isHost() then
+function nextSet()
+	local ticker = 0
+	local toSend = ""
+	while ticker < PACKETTIME do
 		currentLine = song["song"][line]
-		pings.playline(instruments, currentLine)
 		line = line + 1
+		ticker = ticker + 1
 		if song["song"][line] == nil then
 			stop()
+			break
 		end
+		if currentLine[1] ~= nil then
+			toSend = toSend .. ";" .. tostring(ticker - 1) .. "-"
+		end
+		for _, note in ipairs(currentLine) do
+			toSend = toSend .. tostring(note[1]) .. note[2] .. note[3] .. "-"
+		end
+		
+	end
+	pings.sentCurrentSet(toSend)
+	ticker = 0
+end
+
+function playLine()
+	local play = parseTime()
+	if play ~= nil then
+		local toPlay = parse(play)
+		if toPlay ~= nil then
+			for _, note in ipairs(toPlay) do
+				if note ~= nil then
+					sounds:playSound("block.note_block." .. instruments[note[1]], player:getPos() + vec(0, 0.1, 0), 1, (pitch[note[2]] * 2^(note[3] - 4)), false)
+				end
+			end
+		end
+	end
+	_nextNote = _nextNote + 1
+	if _nextNote >= PACKETTIME and not endsong then
+		nextSet()
+		_nextNote = 0
+		_endtime = 2
+		_endpos = 1
 	end
 end
 
-function pings.playline(list, currentline)
-	local instruments = list
-	for _, note in ipairs(currentline) do
-			sounds:playSound("block.note_block." .. instruments[note[1]], player:getPos() + vec(0, 0.1, 0), 1, (pitch[note[2]] * 2^(note[3] - 4)), false)
+function parse(pos)
+	local toPlayStart = string.find(currentSet, ";" .. pos)
+	if toPlayStart == nil then
+		toPlayStart = -1
 	end
+	local toPlayEnd = string.find(currentSet, ";", toPlayStart + 1)
+	if toPlayEnd == nil then
+		toPlayEnd = -1
+	end
+	local toPlayString = string.sub(currentSet, toPlayStart, toPlayEnd)
+	local result = {}
+	for note in string.gmatch(toPlayString, "%-%d+%a+%d+%-") do
+		if note ~= nil then
+			local noteTable = {
+				tonumber(string.match(note, "%d+")), 
+				string.match(note, "%a+"), 
+				tonumber(string.sub(string.match(note, "%d+%-"), 0, -2))
+			}
+			result[#result + 1] = noteTable 
+		end
+	end
+	return result
+end
+
+function parseTime()
+	local startpos = _endtime
+	local nextpos = string.find(currentSet, ";", startpos + 1, false)
+	if nextpos == nil then
+		nextpos = -1
+	end
+	local toPlayString = string.sub(currentSet, startpos, nextpos)
+	local lineNumber = string.match(toPlayString, "%d+")
+	if tonumber(lineNumber) ~=nil then
+		if _nextNote == tonumber(lineNumber) then
+			_endtime = nextpos + 1
+			return lineNumber
+		end
+	end
+	return nil
+end
+
+function pings.sendInstruments(list)
+	instruments = list
+end
+
+function pings.sentCurrentSet(string)
+	currentSet = string
 end
