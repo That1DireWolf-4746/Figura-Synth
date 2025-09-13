6d737b95b4ff86ce1dd510f701566cba986388ae
That1DireWolf-4746 <vwgchan@gmail.com>
2025-09-13 02:58:49 -0700

    Update Synth.lua
    cleanup

--- Synth.lua
+++ Synth.lua
@@ -77,13 +77,12 @@ function nextSet()
 end
 
 function playLine()
-	local play = parseTime()
-	if play ~= nil then
-		local toPlay = parse(play)
-		if toPlay ~= nil then
-			for _, note in ipairs(toPlay) do
-				sounds["block.note_block." .. instruments[note[1]]]:play():setPos(player:getPos() + vec(0, 0.1, 0)):setPitch((pitch[note[2]] * 2^(note[3] - 4)))
-			end
+	local toPlay = parse()
+	if toPlay ~= nil then
+		for _, note in ipairs(toPlay) do
+			sounds["block.note_block." .. instruments[note[1]]]:play()
+					:setPos(player:getPos() + vec(0, 0.1, 0))
+					:setPitch((pitch[note[2]] * 2^(note[3] - 4)))
 		end
 	end
 	_nextNote = _nextNote + 1
@@ -103,37 +102,31 @@ function setLengthBelowLimit()
 	return count < 45
 end
 
-function parse(pos)
-	local toPlayEnd = string.find(currentSet, ";", 2)
-	if toPlayEnd == nil then
-		toPlayEnd = -1
-	end
-	local toPlayString = string.sub(currentSet, 1, toPlayEnd)
-	local result = {}
-	for note in string.gmatch(toPlayString, "%d+%a+%d+%-") do
-		if note ~= nil then
-			local noteTable = {
-				tonumber(string.match(note, "%d+")), 
-				string.match(note, "%a+"), 
-				tonumber(string.sub(string.match(note, "%d+%-"), 0, -2))
-			}
-			result[#result + 1] = noteTable 
-		end
-	end
-	currentSet = string.sub(currentSet, toPlayEnd, -1)
-	return result
-end
-
-function parseTime()
-	local nextpos = string.find(currentSet, "%-", 2, false)
+function parse()
+	local nextpos = string.find(currentSet, ";", 2)
 	if nextpos == nil then
 		nextpos = -1
 	end
-	local lineNumber = string.sub(currentSet, 2, nextpos - 1)
-	if tonumber(lineNumber) ~=nil then
-		if _nextNote == tonumber(lineNumber) then
-			return lineNumber
+	local endOfIndex = string.find(currentSet, "%-", 2)
+	if endOfIndex == nil then
+		endOfIndex = 0
+	end
+	local lineNumber = string.sub(currentSet, 2, endOfIndex - 1)
+	if _nextNote == tonumber(lineNumber) then
+		local toPlayString = string.sub(currentSet, 1, nextpos)
+		local result = {}
+		for note in string.gmatch(toPlayString, "%d+%a+%d+%-") do
+			if note ~= nil then
+				local noteTable = {
+					tonumber(string.match(note, "%d+")), 
+					string.match(note, "%a+"), 
+					tonumber(string.sub(string.match(note, "%d+%-"), 0, -2))
+				}
+				result[#result + 1] = noteTable 
+			end
 		end
+		currentSet = string.sub(currentSet, nextpos, -1)
+		return result
 	end
 	return nil
 end
